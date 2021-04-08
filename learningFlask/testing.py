1120efb2848002d92cf158f3973982e9b7b184f3
Sanj-1873 <sanjnac@gmail.com>
2021-04-08 18:22:40 +0530

    vid 3

--- learningFlask/testing.py
+++ learningFlask/testing.py
@@ -4,10 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/<name>")
-def home(name):
-    return render_template('index.html', content=['song1', 'song2', 'song3'])
+def home():
+    return render_template('base.html', content=['song1', 'song2', 'song3'])
 
+@app.route("/page2")
+def page2():
+    return render_template('test.html')
 
 
 if __name__ == "__main__":
-    app.run() 
+    app.run(debug=True) 
